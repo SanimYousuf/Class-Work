17dc93a66fb41468d5c440c30bc7ca31ff0afa7e
SanimYousuf <sanimyousuf02@gmail.com>
2024-09-17 10:52:01 +0600

    first commit

---
+++ linear-search.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+
+    cout<<"Enter Your values: "<<endl;
+    for(int i=0; i<n; i++)
+    {
+        cin>>arr[i];
+    }
+
+    cout<<"Enter target value: "<<endl;
+    int target;
+    cin>>target;
+
+    int flag = 0;
+    int ind = 0;
+
+    for(int i=0; i<n; i++)
+    {
+        if(target==arr[i])
+        {
+            flag = 1;
+            ind = i+1;
+            break;
+        }
+    }
+
+    if(flag)
+    {
+        cout<<"Target found at "<<ind<<"-th position"<<endl;
+    }
+    else{
+        cout<<"Target not found"<<endl;
+    }
+}
